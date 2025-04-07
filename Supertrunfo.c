8547bf44584803063115dfe4e60b17ae12c03969
GabrielFer1504 <gabrielbim6@gmail.com>
2025-04-07 11:23:23 +0000

    Eu criei o printf

---
+++ Supertrunfo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Desafio Cartas Super Trunfo Paises! em "C" - Nivel Novato.
+
+     int main(){
+         printf("Desafio Cartas Super Trunfo - Novato\n");
+        
+    
+
+    return 0;
+ }
+// Encerramos o código com o comando "return 0" e com o fechamento das chaves ( " } " ) para finalizar a leitura do código.
