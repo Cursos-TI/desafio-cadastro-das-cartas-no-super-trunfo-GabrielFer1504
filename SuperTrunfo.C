e191001080ba16e4a4e36ba7acceb6fd8a48e701
GabrielFer1504 <gabrielbim6@gmail.com>
2025-04-16 10:26:19 +0000

    supertrunfo-novato

--- SuperTrunfo.C
+++ SuperTrunfo.C
@@ -16,8 +16,7 @@ int População1, População2;
 float Área1, Área2;
 float PIB1, PIB2;
 int Numero_De_Pontos_Turisticos1, Numero_De_Pontos_Turisticos2; 
-float Densidade_Populacional1, Densidade_Populacional2;
-float PIB_per_capita1, PIB_per_capita2;
+
 
 // Cadastro Das Cartas.
 
@@ -45,18 +44,6 @@ scanf("%f", &PIB1);
 printf("Numero de Pontos Turísticos: \n");
 scanf("%d", &Numero_De_Pontos_Turisticos1);
 
-printf("Densidade Populacional: \n");
-scanf("%f", &Densidade_Populacional1);
-
-printf("PIB per capita: \n");
-scanf("%f", &PIB_per_capita1);
-
-
-// Cálculos para Carta 1:
-
-PIB_per_capita1 = PIB1 / População1;
-Densidade_Populacional1 = População1 / Área1;
-
 
 // Exibindo dados da Carta 1:
 
@@ -68,8 +55,6 @@ printf("População: %d\n", População1);
 printf("Área: %.2f\n", Área1);
 printf("PIB: %.2f\n", PIB1);
 printf("Numero De Pontos Turísticos: %d\n", Numero_De_Pontos_Turisticos1);
-printf("Densidade Populacional: %.2f\n", Densidade_Populacional1);
-printf("PIB per capita: %f\n", PIB_per_capita1);
 
 
 // CARTA 02:
@@ -96,18 +81,6 @@ scanf("%f", &PIB2);
 printf("Numero de Pontos Turísticos: \n");
 scanf("%d", &Numero_De_Pontos_Turisticos2);
 
-printf("Densidade Populacional: \n");
-scanf("%f", &Densidade_Populacional2);
-
-printf("PIB per capita: \n");
-scanf("%f", &PIB_per_capita2);
-
-
-// Cálculos para Carta 2:
-
-PIB_per_capita2 = PIB2 / População2;
-Densidade_Populacional2 = População2 / Área2;
-
 
 // Exibindo dados da Carta 2:
 
@@ -119,8 +92,6 @@ printf("População: %d\n", População2);
 printf("Área: %.2f\n", Área2);
 printf("PIB: %.2f\n", PIB2);
 printf("Numero De Pontos Turísticos: %d\n", Numero_De_Pontos_Turisticos2);
-printf("Densidade Populacional: %.2f\n", Densidade_Populacional2);
-printf("PIB per capita: %f\n", PIB_per_capita2);
 
 return 0; 
 }
